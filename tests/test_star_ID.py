25f7dd6c5b1e1828bcb20ca3265e4b94dcef49dd
brizo <nathanielheidt@gmail.com>
2015-04-20 15:18:14 -0700

    changed the program to ensure that any wuad has the AB star as the two stars with the biggest distance between them.

--- tests/test_star_ID.py
+++ tests/test_star_ID.py
@@ -9,7 +9,7 @@ keypts, im = ps.getStarPointsFromFile('images/N40_30W120_35_5999_2015-02-22-1-10
 
 image = cv2.imread('images/N40_30W120_35_5999_2015-02-22-1-10-14.jpg')
 
-pts = ps.getBrightestPoints(keypts, 90)
+pts = ps.getBrightestPoints(keypts, 70)
 
 im_with_keypoints = cv2.drawKeypoints(image, pts, np.array([]), (0,255,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
@@ -28,7 +28,25 @@ for star in brightestStars:
 
 print maxx, maxy
 starRepr = ps.starRepresentation(maxy, maxx, 300, 300)
-starRepr.drawQuads(quads[3:4])
-starRepr.drawUnitCircles(quads[3:4])
+starRepr.drawQuads(quads[0::1])
+#starRepr.drawUnitCircles(quads[0::1])
 starRepr.itsFullOfStars(brightestStars)
-starRepr.showImage()
+starRepr.showImage("stars")
+
+maxx = 0
+maxy = 0
+for pt in pts:
+	if pt.pt[0] > maxx:
+		maxx = pt.pt[0]*1 + 10
+	if pt.pt[1] > maxy:
+		maxy = pt.pt[1]*1 + 10
+
+imQuads = ps.indexImageStars(pts, '40.5', '-120.593332', 100, '2015/02/22 01:10:14')
+
+fromImage = ps.starRepresentation(maxy, maxx, 1, 1)
+fromImage.drawQuads(imQuads)
+fromImage.fillStarsWithPts(pts)
+fromImage.showImage("image")
+
+
+cv2.waitKey(0)

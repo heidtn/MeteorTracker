249cc411f04268a96e723691b5e6b6a52a0da54a
nathaniel heidt <nathanielheidt@gmail.com>
2016-01-12 08:26:03 +0000

    fixed rpi camera issues.  Was accidentally opening camera twice.

--- MeteorTracker/camera.py
+++ MeteorTracker/camera.py
@@ -2,10 +2,13 @@ import os
 import cv2
 try:
 	import picamera
+	import picamera.array
 except Exception:
 	pass
 import io
 import numpy as np
+import time
+from fractions import Fraction
 
 class camera:
 	def __init__(self, source=None):
@@ -21,6 +24,12 @@ class camera:
 
 		if self.camera_type == 'picam':
 			self.camera = picamera.PiCamera()
+			time.sleep(2)
+			self.camera.resolution = (1280, 720)
+			self.camera.framerate = Fraction(1, 6)
+			self.camera.exposure_mode = 'off'
+			self.camera.shutter_speed = 6000000
+			self.camera.ISO = 800
 		elif self.camera_type == 'cvcam':
 			self.camera = cv2.VideoCapture(0)
 		elif self.camera_type == 'custom':
@@ -28,14 +37,11 @@ class camera:
 
 	def getFrame(self):
 		if self.camera_type == 'picam':
-			with picamera.PiCamera() as camera:
-				self.camera.start_preview()
-				time.sleep(2)
-				with picamera.array.PiRGBArray(camera) as stream:
-					self.camera.capture(stream, format='bgr')
-					# At this point the image is available as stream.array
-					image = stream.array
-					return image
+			with picamera.array.PiRGBArray(self.camera) as stream:
+				self.camera.capture(stream, format='rgb')
+				image = stream.array
+				print "img returned"
+				return image
 		elif self.camera_type == 'cvcam':
 			ret, frame = self.camera.read()
 			return frame

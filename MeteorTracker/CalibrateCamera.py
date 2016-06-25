d3b014805956eaa0dbc1910a5763be701f20cce6
Nathan Heidt <nathanielheidt@gmail.com>
2016-06-25 00:39:59 -0700

    cleanup

--- MeteorTracker/CalibrateCamera.py
+++ MeteorTracker/CalibrateCamera.py
@@ -73,25 +73,7 @@ def calibrateCamera():
 
 	print "Total reprojection error: ", mean_error/len(objpoints)
 
-	#to undistort, use following:
-	#h,  w = img.shape[:2]
-	#newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
-	# undistort
-	#dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
-
-	# crop the image
-	#x,y,w,h = roi
-	#dst = dst[y:y+h, x:x+w]
-	#cv2.imwrite('calibresult.png',dst)
-
-	# undistort using curved path remapping
-	#mapx,mapy = cv2.initUndistortRectifyMap(mtx,dist,None,newcameramtx,(w,h),5)
-	#dst = cv2.remap(img,mapx,mapy,cv2.INTER_LINEAR)
-
-	# crop the image
-	#x,y,w,h = roi
-	#dst = dst[y:y+h, x:x+w]
-	#cv2.imwrite('calibresult.png',dst)
+	
 
 def undistortImage(img, camMatrix, distCoeff):
 	h,  w = img.shape[:2]
